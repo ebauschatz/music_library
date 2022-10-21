c8c530bdf72e6f8099caf27e2e3f2f6a006ac2b7
ebauschatz <eaenglish113@gmail.com>
2022-10-21 11:40:13 -0500

    feat: adding a post endpoint to create new songs

--- songs/views.py
+++ songs/views.py
@@ -5,9 +5,15 @@ from rest_framework import status
 from .models import Song
 from .serializers import SongSerializer
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def songs_list(request):
     if request.method == 'GET':
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
