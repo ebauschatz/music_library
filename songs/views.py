e42a860ec388f80d3b1d094b6dd369cf071130b9
ebauschatz <eaenglish113@gmail.com>
2022-10-21 11:53:57 -0500

    feat: adding endpoints for updating / deleting a song by ID

--- songs/views.py
+++ songs/views.py
@@ -18,9 +18,17 @@ def songs_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def song_detail(request, pk):
     song = get_object_or_404(Song, pk = pk)
     if request.method == 'GET':
         serializer = SongSerializer(song)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
