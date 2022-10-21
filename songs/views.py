abf19f97c16ba17a893aab8211d9ec4e52bdb528
ebauschatz <eaenglish113@gmail.com>
2022-10-21 11:27:54 -0500

    feat: adding endpoint to return all songs

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from .models import Song
+from .serializers import SongSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def songs_list(request):
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
